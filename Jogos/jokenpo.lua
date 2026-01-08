c20564e1db14dc0eff9e2f36c01ea8a13e43b5d6
João W. <78827094+JoWttk@users.noreply.github.com>
2026-01-08 08:15:01 +0000

    idk how to use github on replit... i miss my pc

--- Jogos/jokenpo.lua
+++ Jogos/jokenpo.lua
@@ -1,41 +1,37 @@
-function Jokenpo()
-    math.randomseed(os.time())
-    
-    local escolhas = {
-    pedra = true,
-    papel = true,
-    tesoura = true
-    }
-    
-    local lista = { "pedra", "papel", "tesoura" }
-    
-    local vence = {
-    pedra   = "tesoura",
-    tesoura = "papel",
-    papel   = "pedra"
-    }
-    
-    while true do
-    print("\nEscolha entre pedra, papel e tesoura:")
-    local mao = io.read()
-    
-    if not escolhas[mao] then
-      print("Escolha inválida. Tente novamente.")
-    else
-      local IA = lista[math.random(3)]
-      print("A IA escolheu: " .. IA)
-    
-      if mao == IA then
-        print("Empate! Jogando novamente...")
-      elseif vence[mao] == IA then
-        print("Você ganhou!")
-        break
-      else
-        print("Você perdeu!")
-        break
-      end
-    end
-    end
-end
+math.randomseed(os.time())
 
-Jokenpo()
+local escolhas = {
+pedra = true,
+papel = true,
+tesoura = true
+}
+
+local lista = { "pedra", "papel", "tesoura" }
+
+local vence = {
+pedra   = "tesoura",
+tesoura = "papel",
+papel   = "pedra"
+}
+
+while true do
+print("\nEscolha entre pedra, papel e tesoura:")
+local mao = io.read()
+
+if not escolhas[mao] then
+  print("Escolha inválida. Tente novamente.")
+else
+  local IA = lista[math.random(3)]
+  print("A IA escolheu: " .. IA)
+
+  if mao == IA then
+    print("Empate! Jogando novamente...")
+  elseif vence[mao] == IA then
+    print("Você ganhou!")
+    break
+  else
+    print("Você perdeu!")
+    break
+  end
+end
+end
